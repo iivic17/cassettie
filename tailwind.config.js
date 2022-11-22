79338fec3e6eafe47775fa34ba6e1a934ba45dad
iivic17 <ilijaivic17@gmail.com>
2022-11-22 18:13:30 +0100

    Set up initial tailwindcss theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,28 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      red: "rgb(252, 71, 71)",
+      "blue-dark": "rgb(16, 20, 30)",
+      "blue-medium": "rgb(22, 29, 47)",
+      "blue-light": "rgb(90, 105, 143)",
+      white: "rgb(255, 255, 255)",
+    },
+    fontFamily: {
+      sans: ["Outfit", "sans-serif"],
+    },
+    fontWeight: {
+      light: "300",
+      medium: "500",
+    },
+    fontSize: {
+      "body-s": "0.8125rem",
+      "body-m": "0.9375rem",
+      "heading-xs": "1.125rem",
+      "heading-s": "1.5rem",
+      "heading-m": "1.5rem",
+      "heading-l": "2rem",
+    },
   },
   plugins: [
     require("@tailwindcss/forms"),
