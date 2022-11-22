e8b9bd8eb8ead065e0653fb89c4632f7c52196a5
iivic17 <ilijaivic17@gmail.com>
2022-11-22 17:49:06 +0100

    Connect tailwindcss forms, aspect-ratio, typography and line-camp plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,10 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+    require("tailwindcss-children"),
+  ],
 };
