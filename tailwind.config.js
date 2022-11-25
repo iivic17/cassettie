584cf12889b8df6a63797847e1c488b16e6fed64
iivic17 <ilijaivic17@gmail.com>
2022-11-25 23:30:35 +0100

    Wireframe initial route set up

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,6 +34,6 @@ module.exports = {
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/typography"),
     require("@tailwindcss/line-clamp"),
-    require('tailwind-children'),
+    require("tailwind-children"),
   ],
 };
