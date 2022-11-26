a04ecb50487fb17287138d4e1a55f1c51884a640
iivic17 <ilijaivic17@gmail.com>
2022-11-26 18:12:45 +0100

    Create initial ui for sign-up page

--- pages/sign-up/index.tsx
+++ pages/sign-up/index.tsx
@@ -1,3 +1,29 @@
-const SignUp = () => <div>SignUp</div>;
+import Link from "next/link";
+import { AuthTemplate } from "../../components";
+import { Input, TextBody, TextHeading, Button } from "../../design";
+
+const SignUp = () => {
+  return (
+    <AuthTemplate>
+      <TextHeading variant="l" className="text-white">
+        Sign Up
+      </TextHeading>
+      <div className="mt-8 flex flex-col gap-3">
+        <Input placeholder="Email address" type="email" />
+        <Input placeholder="Password" type="password" />
+        <Input placeholder="Repeat password" type="password" />
+      </div>
+      <Button className="mt-10">Create an account</Button>
+      <div className="mt-7 flex flex-row items-center justify-center gap-2">
+        <TextBody variant="m" className="text-white">
+          Already have an account?
+        </TextBody>
+        <TextBody variant="m" className="text-red" as={Link} href="/login">
+          Login
+        </TextBody>
+      </div>
+    </AuthTemplate>
+  );
+};
 
 export default SignUp;
